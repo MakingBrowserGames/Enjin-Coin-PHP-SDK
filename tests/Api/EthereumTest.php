9f6742724451ddc8b6209319bdf4d06b445d334e
Damien Gallagher <damien.gallagher@gmail.com>
2018-01-23 19:19:40 +0000

    Added mockery in order to mock the ethereum service

--- tests/Api/EthereumTest.php
+++ tests/Api/EthereumTest.php
@@ -39,7 +39,11 @@ final class EthereumTest extends MockeryTestCase {
 	}
 	
 	public function testGetBalances_InvalidEthereumAddresses(): void {
-		$addresses = ['1', '2', '3'];
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('validateAddress')->once()->with($this->ethereum_address)->andReturn(false);
+		
+		$addresses = [$this->ethereum_address];
 		$result = $this->ethereumApi->getBalances($addresses);
 		$this->assertEmpty($result);
 	}
@@ -55,6 +59,86 @@ final class EthereumTest extends MockeryTestCase {
 		$result = $this->ethereumApi->getBalances($addresses);
 		$this->assertNotEmpty($result);
 	}
+	
+	public function testEstimateGas(): void {
+		
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('msg')->once()->with(anyArgs())->andReturn(true);
+		
+		$ethCall = ["ethCall"];
+		$result = $this->ethereumApi->estimateGas($ethCall);
+		$this->assertNotEmpty($result);
+	}
+	
+	/**
+     * @expectedException Exception
+     */	
+	public function testGetTransactionCount_InvalidAddress(): void {
+		$address = 'address' . rand(100000000, 999999999);
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('validateAddress')->once()->with($address)->andReturn(false);
+		
+		$result = $this->ethereumApi->getTransactionCount($address);
+		$this->assertEmpty($result);
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('Invalid address.');
+	}
+	
+	public function testGetTransactionCount_Success(): void {
+		$address = 'address' . rand(100000000, 999999999);
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('validateAddress')->once()->with($address)->andReturn(true);
+		$mockEthereumService->shouldReceive('msg')->once()->with(anyArgs())->andReturn(true);
+		
+		$result = $this->ethereumApi->getTransactionCount($address);
+		$this->assertNotEmpty($result);
+	}
+	
+	public function testGetTransactionByHash(): void {
+		
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('msg')->once()->with(anyArgs())->andReturn(true);
+		
+		$hash = 'hash' . rand(100000000, 999999999);
+		$result = $this->ethereumApi->getTransactionByHash($hash);
+		$this->assertNotEmpty($result);
+	}	
+	
+	public function testSendRawTransaction(): void {
+		
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('msg')->once()->with(anyArgs())->andReturn(true);
+		
+		$data = 'data' . rand(100000000, 999999999);
+		$result = $this->ethereumApi->sendRawTransaction($data);
+		$this->assertNotEmpty($result);
+	}
+	
+	public function testVerifySig(): void {
+		
+		$address = 'address' . rand(100000000, 999999999);
+		$hash = 'hash' . rand(100000000, 999999999);
+		$message = 'message' . rand(100000000, 999999999);
+
+		$result = $this->ethereumApi->verifySig($address, $hash, $message);
+		$this->assertEmpty($result);
+	}	
+	
+	public function testTestMethod(): void {
+		
+		$mockEthereumService = Mockery::mock("overload:EnjinCoin\Ethereum");
+
+		$mockEthereumService->shouldReceive('test')->once()->with(anyArgs())->andReturn(true);
+		
+		$result = $this->ethereumApi->test();
+		$this->assertTrue($result);
+	}	
+	
 	
 	public function tearDown()
     {
